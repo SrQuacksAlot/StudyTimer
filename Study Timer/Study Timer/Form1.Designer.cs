f52a76a4e56457cc61045c681db55adc4265551d
Khalil Khalil <52632838+SrQuacksAlot@users.noreply.github.com>
2024-03-26 16:06:24 +0100

    Add files via upload

---
+++ Study Timer/Study Timer/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Study_Timer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Progressbar = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Title = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.session_label = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.Progressbar);
+            this.panel1.Location = new System.Drawing.Point(210, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 70);
+            this.panel1.TabIndex = 0;
+            // 
+            // Progressbar
+            // 
+            this.Progressbar.BackColor = System.Drawing.Color.DarkOrange;
+            this.Progressbar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Progressbar.Location = new System.Drawing.Point(0, 0);
+            this.Progressbar.Name = "Progressbar";
+            this.Progressbar.Size = new System.Drawing.Size(26, 71);
+            this.Progressbar.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("SamsungOneUILatin 700", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(527, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 36);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Study time in minutes";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(527, 220);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(97, 40);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Title
+            // 
+            this.Title.BackColor = System.Drawing.Color.DarkOrange;
+            this.Title.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Title.Font = new System.Drawing.Font("SamsungOneUILatin 700", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.Black;
+            this.Title.Location = new System.Drawing.Point(576, 65);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(100, 40);
+            this.Title.TabIndex = 2;
+            this.Title.Text = "Study";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("SamsungOneUILatin 700", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(630, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 36);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "break time in minutes";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox2.Location = new System.Drawing.Point(630, 220);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(97, 40);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SlateBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(1016, 105);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(73, 73);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Location = new System.Drawing.Point(573, 263);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(111, 47);
+            this.infoLabel.TabIndex = 8;
+            this.infoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // session_label
+            // 
+            this.session_label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.session_label.Font = new System.Drawing.Font("SamsungOneUILatin 700", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.session_label.ForeColor = System.Drawing.Color.Black;
+            this.session_label.Location = new System.Drawing.Point(208, 56);
+            this.session_label.Name = "session_label";
+            this.session_label.Size = new System.Drawing.Size(135, 49);
+            this.session_label.TabIndex = 9;
+            this.session_label.Text = "Completed Study Sessions: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(1264, 336);
+            this.Controls.Add(this.session_label);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label Progressbar;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label session_label;
+    }
+}
+
---
+++ Study Timer/Study Timer/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Study_Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        float study_mins;
+        float break_mins;
+        float study_secs;
+        float break_secs;
+        //Point formPos;
+        //Point timeLabelPos;
+        //Point barPos;
+        //Point barSize;
+        //Point formSize;
+        //Point timeLabelSize;
+
+        //SystemColors[] colors;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Progressbar.Width= 0;
+            Title.Text = "Study";
+            
+        }
+        SoundPlayer anouncementSound = new SoundPlayer(@"C:\Users\khali\Downloads\call-to-attention-123107.wav");
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+        int sessions = 0;
+        float lost_time = 0;
+        float remainder;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            remainder = panel1.Width % 60;
+            lost_time += remainder / 60;
+            if (lost_time >= 1)
+            {
+                Progressbar.Width += Convert.ToInt16(panel1.Width / 60) + 1;
+                lost_time--;
+            }
+            else
+            {
+                Progressbar.Width += Convert.ToInt16(panel1.Width / 60);
+            }
+            if (Progressbar.Width >= panel1.Width)
+            {
+                anouncementSound.Play();
+                if (Title.Text == "Study")
+                {
+                    sessions += 1;
+                    session_label.Text = "Completed Study Sessions: " + Convert.ToString(sessions);
+                    timer1.Interval = Convert.ToInt16(1000*break_secs/60);
+                    Title.Text = "Break";
+                }
+                else
+                {
+                    Title.Text = "Study";
+                    timer1.Interval = Convert.ToInt16(1000*study_secs/60);
+                }
+                Progressbar.Width = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            bool textbox1 = int.TryParse(richTextBox1.Text, out _);
+            bool textbox2 = int.TryParse(richTextBox1.Text, out _);
+            if (textbox1 && textbox2 && Convert.ToInt16(richTextBox1.Text) != 0 && Convert.ToInt16(richTextBox2.Text) != 0)
+            {
+                Progressbar.Width = 0;
+                study_mins = Convert.ToInt16(richTextBox1.Text);
+                break_mins = Convert.ToInt16(richTextBox2.Text);
+                study_secs = 60 * study_mins; // study time in seconds
+                break_secs = 60 * break_mins; // break time in seconds
+
+                timer1.Interval = Convert.ToInt16(1000*study_secs/60);
+                timer1.Enabled= true;
+                infoLabel.Text = "";
+            }
+            else
+            {
+                infoLabel.Text = "the textboxes have not been filled correctly";
+            }
+            
+        }
+    }
+}
